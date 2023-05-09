774659c5c9a89775e6c6e875105481e6538f1bdd
sh990920 <sh990920@gmail.com>
2023-05-09 21:53:56 +0900

    05/09

---
+++ Day09/Ex01_sklearn-iris.py
@@ -0,0 +1,14 @@
+'''
+주피터 노트북 설치
+pip install notebook
+
+cmd / 터미널에서 실행
+jupyter notebook
+
+pip install numpy
+pip install pandas
+pip install scikit-learn
+'''
+import sklearn
+
+print(sklearn.__version__)
